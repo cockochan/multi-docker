896d43c13ae674f17c88df7699e73383fb5f6798
Constantin Dimitrenco <constantin.dimitrenco@gmail.com>
2021-03-08 13:10:24 +0000

    copypaste

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
+
 function App() {
   return (
     <Router>
-      <hz></hz>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -17,8 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-
-            Learn React
+            Learn React 3
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
